34bdc5ac62cc0cb41acf80d02c0936e1809e0336
Cormac O'Brien <cormac@c-obrien.org>
2017-11-06 13:13:57 -0600

    Add render-to-texture support

--- main.cpp
+++ main.cpp
@@ -25,6 +25,7 @@
 #include <cstdint>
 #include <cstdio>
 #include <map>
+#include <random>
 #include <vector>
 
 #include <GL/gl3w.h>
@@ -37,6 +38,12 @@
 
 #define TINYOBJLOADER_IMPLEMENTATION
 #include "tiny_obj_loader.hpp"
+
+#define KERNEL_SIZE 32
+#define NOISE_SIZE_1D 4
+#define NOISE_SIZE (NOISE_SIZE_1D * NOISE_SIZE_1D)
+
+#define LERP(a, b, f) ((a) * (1.0f - (f)) + (b) * (f))
 
 #define INFO(M, ...)                                                           \
     fprintf(stderr, "[INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
@@ -51,7 +58,7 @@
         size_t ecnt = 0;                                                       \
         while ((err = glGetError()) != GL_NO_ERROR) {                          \
             fprintf(stderr, "[OPENGL] (%s:%d) " M ": (%x)\n", __FILE__,        \
-                    __LINE__, err, ##__VA_ARGS__);                             \
+                    __LINE__, ##__VA_ARGS__, err);                             \
             ecnt += 1;                                                         \
         }                                                                      \
         if (ecnt > 0) {                                                        \
@@ -111,7 +118,8 @@ const char *const frag_src = R"glsl(
 in vec3 f_normal;
 in vec2 f_texcoord;
 
-out vec4 color;
+layout (location = 0) out vec4 out_color;
+layout (location = 1) out vec4 out_normal;
 
 uniform bool diff;
 uniform bool mask;
@@ -122,12 +130,62 @@ void main() {
     if (mask && texture(mask_tex, f_texcoord).r < 0.5) {
         discard;
     } else if (diff) {
-        color = texture(diff_tex, f_texcoord);
+        out_color = texture(diff_tex, f_texcoord);
     } else {
-        color = vec4(0.0f, 0.0f, 0.0f, 1.0f);
+        out_color = vec4(0.0f, 0.0f, 0.0f, 1.0f);
     }
+
+    out_normal = vec4(f_normal, 1.0f);
 };
 )glsl";
+
+const char *const fullscreen_vert_src = R"glsl(
+#version 330
+
+layout (location = 0) in vec2 v_position;
+layout (location = 1) in vec2 v_texcoord;
+
+out vec2 f_texcoord;
+
+void main() {
+    f_texcoord = v_texcoord;
+    gl_Position = vec4(v_position.xy, 0.0f, 1.0f);
+}
+)glsl";
+
+const char *const fullscreen_frag_src = R"glsl(
+#version 330
+
+in vec2 f_texcoord;
+
+out vec4 color;
+
+uniform sampler2D tex;
+
+void main() {
+    color = texture(tex, f_texcoord);
+}
+)glsl";
+
+/*
+ * Vertex data for rendering a texture to fullscreen, stored as [x y | s t]
+ */
+const GLfloat fullscreen_vertex[6][4] = {
+    {-1.0f, -1.0f, 0.0f, 0.0f}, // bottom left
+    {1.0f, 1.0f, 1.0f, 1.0f},   // top right
+    {-1.0f, 1.0f, 0.0f, 1.0f},  // top left
+    {-1.0f, -1.0f, 0.0f, 0.0f}, // bottom left
+    {1.0f, -1.0f, 1.0f, 0.0f},  // bottom right
+    {1.0f, 1.0f, 1.0f, 1.0f}    // top right
+};
+
+void APIENTRY opengl_error_callback(GLenum source, GLenum type, GLuint id,
+                                    GLenum severity, GLsizei length,
+                                    const GLchar *msg, const void *user_param) {
+    if (severity != GL_DEBUG_SEVERITY_NOTIFICATION) {
+        printf("%s\n", msg);
+    }
+}
 
 GLuint new_shader(const GLenum type, const char *src) {
     GLuint shader = glCreateShader(type);
@@ -178,67 +236,18 @@ GLuint new_program(size_t shader_count, const GLuint *shaders) {
     return program;
 }
 
-int main(int argc, char *argv[]) {
-    glfwInit();
-    glfwWindowHint(GLFW_SAMPLES, 16);
-    GLFWwindow *window =
-        glfwCreateWindow(1920, 1080, "objview", glfwGetPrimaryMonitor(), NULL);
-    glfwMakeContextCurrent(window);
-
-    if (gl3wInit() != 0) {
-        ERROR("Failed to initialize OpenGL context");
-    }
-
-    if (!gl3wIsSupported(4, 3)) {
-        INFO("OpenGL debug output unavailable (OpenGL 4.3 not supported).");
-    } else {
-        INFO("Enabling OpenGL debug output.");
-        glEnable(GL_DEBUG_OUTPUT);
-    }
-
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-    glFrontFace(GL_CCW);
-
-    glEnable(GL_DEPTH_TEST);
-    glDepthMask(GL_TRUE);
-    glDepthFunc(GL_LEQUAL);
-    glDepthRange(0.0f, 1.0f);
-
-    glEnable(GL_MULTISAMPLE);
-
+/*
+ * Load all textures specified by the MTL data given in `materials` and store
+ * them in `textures`
+ */
+bool create_textures(std::vector<Texture> &textures,
+                     std::vector<tinyobj::material_t> &materials) {
     /*
-     * Load OBJ file
-     */
-    tinyobj::attrib_t attrib;
-    std::vector<tinyobj::shape_t> shapes;
-    std::vector<tinyobj::material_t> materials;
-    std::vector<GLuint> texture_ids;
-    std::string err_string;
-
-    bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &err_string,
-                                "crytek-sponza/sponza.obj", "crytek-sponza/");
-    if (!err_string.empty()) {
-        INFO("Possible error while loading OBJ file:\n%s", err_string.c_str());
-    }
-
-    if (!ret) {
-        ERROR("Error while loading OBJ file.");
-        exit(EXIT_FAILURE);
-    }
-
-    /*
-     * Add default material
-     */
-    materials.push_back(tinyobj::material_t());
-
-    /*
-     * Load diffuse texture files
+     * Query anisotropic filtering
      */
     float aniso = 1.0f;
     glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY, &aniso);
 
-    std::vector<Texture> textures;
     for (size_t i = 0; i < materials.size(); i++) {
         Texture tex = Texture{.diff = 0, .mask = 0, .bump = 0, .spec = 0};
 
@@ -253,8 +262,14 @@ int main(int argc, char *argv[]) {
 
             if (tex_id == 0) {
                 ERROR("Failed to load diffuse texture from %s", path.c_str());
-                exit(EXIT_FAILURE);
+                return false;
             }
+
+            /*
+             * Enable anisotropic filtering
+             */
+            glBindTexture(GL_TEXTURE_2D, tex_id);
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY, aniso);
 
             tex.diff = tex_id;
         }
@@ -270,6 +285,7 @@ int main(int argc, char *argv[]) {
 
             if (tex_id == 0) {
                 ERROR("Failed to load mask texture from %s", path.c_str());
+                return false;
             }
 
             tex.mask = tex_id;
@@ -286,6 +302,7 @@ int main(int argc, char *argv[]) {
 
             if (tex_id == 0) {
                 ERROR("Failed to load bump texture from %s", path.c_str());
+                return false;
             }
 
             tex.bump = tex_id;
@@ -302,6 +319,7 @@ int main(int argc, char *argv[]) {
 
             if (tex_id == 0) {
                 ERROR("Failed to load specular texture from %s", path.c_str());
+                return false;
             }
 
             tex.spec = tex_id;
@@ -310,10 +328,13 @@ int main(int argc, char *argv[]) {
         textures.push_back(tex);
     }
 
-    /*
-     * Reorganize geometry data for uploading to GPU
-     */
-    std::vector<DrawObject> draw_objects;
+    return true;
+}
+
+bool create_draw_objects(std::vector<DrawObject> &draw_objects,
+                         tinyobj::attrib_t &attrib,
+                         std::vector<tinyobj::shape_t> &shapes,
+                         std::vector<tinyobj::material_t> &materials) {
     for (size_t s = 0; s < shapes.size(); s++) {
         /*
          * Shapes can have multiple materials, so generate one DrawObject and
@@ -334,6 +355,7 @@ int main(int argc, char *argv[]) {
             if (mat_id < 0 || mat_id > static_cast<int>(materials.size())) {
                 ERROR("Bad material ID for shape \"%s\"",
                       shapes[s].name.c_str());
+                return false;
             }
 
             /*
@@ -361,6 +383,7 @@ int main(int argc, char *argv[]) {
             } else {
                 ERROR("OBJ file does not provide normals and normal "
                       "calculation is not implemented.");
+                return false;
             }
 
             /*
@@ -429,13 +452,97 @@ int main(int argc, char *argv[]) {
         vertex_data.shrink_to_fit();
     }
 
+    return true;
+}
+
+int main(int argc, char *argv[]) {
+    /*
+     * GLFW setup
+     */
+    glfwInit();
+    glfwWindowHint(GLFW_SAMPLES, 16);
+    GLFWwindow *window =
+        glfwCreateWindow(1920, 1080, "objview", glfwGetPrimaryMonitor(), NULL);
+    glfwMakeContextCurrent(window);
+
+    /*
+     * OpenGL context setup
+     */
+    if (gl3wInit() != 0) {
+        ERROR("Failed to initialize OpenGL context");
+    }
+
+    if (!gl3wIsSupported(4, 3)) {
+        INFO("OpenGL debug output unavailable (OpenGL 4.3 not supported).");
+    } else {
+        INFO("Enabling OpenGL debug output.");
+        glEnable(GL_DEBUG_OUTPUT);
+        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+        glDebugMessageCallback(opengl_error_callback, NULL);
+    }
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthMask(GL_TRUE);
+    glDepthFunc(GL_LEQUAL);
+    glDepthRange(0.0f, 1.0f);
+
+    glEnable(GL_MULTISAMPLE);
+
+    /*
+     * Load OBJ file
+     */
+    tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+    std::string err_string;
+
+    bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &err_string,
+                                "crytek-sponza/sponza.obj", "crytek-sponza/");
+    if (!err_string.empty()) {
+        INFO("OBJ loader log:\n%s", err_string.c_str());
+    }
+
+    if (!ret) {
+        ERROR("Error while loading OBJ file.");
+        exit(EXIT_FAILURE);
+    }
+
+    /*
+     * Add default material
+     */
+    materials.push_back(tinyobj::material_t());
+
+    std::vector<Texture> textures;
+    if (create_textures(textures, materials) == false) {
+        ERROR("Failed to create textures");
+        exit(EXIT_FAILURE);
+    }
+
+    /*
+     * Reorganize geometry data for uploading to GPU
+     */
+    std::vector<DrawObject> draw_objects;
+    if (create_draw_objects(draw_objects, attrib, shapes, materials) == false) {
+        ERROR("Failed to create draw objects");
+        exit(EXIT_FAILURE);
+    }
+
     GLuint vert = new_shader(GL_VERTEX_SHADER, vert_src);
     GLuint frag = new_shader(GL_FRAGMENT_SHADER, frag_src);
     GLuint shaders[2] = {vert, frag};
     GLuint prog = new_program(2, shaders);
+    GLuint fullscreen_vert = new_shader(GL_VERTEX_SHADER, fullscreen_vert_src);
+    GLuint fullscreen_frag =
+        new_shader(GL_FRAGMENT_SHADER, fullscreen_frag_src);
+    GLuint fullscreen_shaders[2] = {fullscreen_vert, fullscreen_frag};
+    GLuint fullscreen_prog = new_program(2, fullscreen_shaders);
     ERROR_OPENGL("Shader compilation and linking failed.");
-    glUseProgram(prog);
 
+    glUseProgram(prog);
     GLuint world_unif = glGetUniformLocation(prog, "world");
     GLuint persp_unif = glGetUniformLocation(prog, "persp");
     GLuint diff_unif = glGetUniformLocation(prog, "diff");
@@ -446,13 +553,134 @@ int main(int argc, char *argv[]) {
     glUniform1i(diff_tex_unif, 0);
     glUniform1i(mask_tex_unif, 1);
 
-    GLuint vao;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
+    GLuint geometry_vao;
+    glGenVertexArrays(1, &geometry_vao);
 
+    /*
+     *
+     */
+    GLuint fullscreen_vao, fullscreen_vbo;
+    glGenVertexArrays(1, &fullscreen_vao);
+    glGenBuffers(1, &fullscreen_vbo);
+    ERROR_OPENGL("Error generating fullscreen vertex array");
+    glBindVertexArray(fullscreen_vao);
+    glBindBuffer(GL_ARRAY_BUFFER, fullscreen_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(fullscreen_vertex), fullscreen_vertex,
+                 GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
-    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat),
+                          reinterpret_cast<void *>(0));
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat),
+                          reinterpret_cast<void *>(2 * sizeof(GLfloat)));
+    glDisableVertexAttribArray(0);
+    glDisableVertexAttribArray(1);
+
+    /*
+     * Set up for SSAO rendering pass. We generate three images: one with
+     * diffuse textures, one with surface normals and one with depth values.
+     */
+
+    GLuint ssao_tex;
+    glActiveTexture(GL_TEXTURE0);
+    glGenTextures(1, &ssao_tex);
+    glBindTexture(GL_TEXTURE_2D, ssao_tex);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1920, 1080, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, 0);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    ERROR_OPENGL("Error generating SSAO texture");
+
+    GLuint ssao_normal_tex;
+    glActiveTexture(GL_TEXTURE1);
+    glGenTextures(1, &ssao_normal_tex);
+    glBindTexture(GL_TEXTURE_2D, ssao_normal_tex);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1920, 1080, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, 0);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    ERROR_OPENGL("Error generating SSAO normal texture");
+
+    GLuint ssao_depth_tex;
+    glActiveTexture(GL_TEXTURE2);
+    glGenTextures(1, &ssao_depth_tex);
+    glBindTexture(GL_TEXTURE_2D, ssao_depth_tex);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT32F, 1920, 1080, 0,
+                 GL_DEPTH_COMPONENT, GL_FLOAT, 0);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    ERROR_OPENGL("Error generating SSAO depth texture");
+
+    GLuint ssao_fbo;
+    glGenFramebuffers(1, &ssao_fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, ssao_fbo);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,
+                           ssao_tex, 0);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D,
+                           ssao_normal_tex, 0);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D,
+                           ssao_depth_tex, 0);
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+        ERROR_OPENGL("SSAO framebuffer object not complete");
+        exit(EXIT_FAILURE);
+    }
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    std::mt19937 gen;
+    std::uniform_real_distribution<GLfloat> zero_one_dist(0.0f, 1.0f);
+    std::uniform_real_distribution<GLfloat> minus_one_one_dist(-1.0f, 1.0f);
+
+    GLfloat kernel[3 * KERNEL_SIZE];
+    for (size_t i = 0; i < KERNEL_SIZE; i++) {
+        /*
+         * Generate points at random across the surface of a hemisphere on the
+         * z-axis with radius r = 1. Initially generate x and y on the range
+         * [-1, 1] and z on the range [0, 1], then normalize the vector
+         */
+        float x = minus_one_one_dist(gen);
+        float y = minus_one_one_dist(gen);
+        float z = zero_one_dist(gen);
+        float mag = sqrtf(x * x + y * y + z * z);
+
+        kernel[3 * i] = x / mag;
+        kernel[3 * i + 1] = y / mag;
+        kernel[3 * i + 2] = z / mag;
+
+        /*
+         * Scatter the points inside the hemisphere using an attenuation
+         * function to place more samples closer to the origin
+         */
+        float scale = (float)i / (float)KERNEL_SIZE;
+        scale = LERP(0.1f, 1.0f, scale);
+        for (size_t c = 0; c < 3; c++) {
+            kernel[3 * i + c] *= scale;
+        }
+    }
+
+    GLfloat noise[3 * NOISE_SIZE];
+    for (size_t i = 0; i < NOISE_SIZE; i++) {
+        /*
+         * Generate a small noise texture used for randomly rotating the sample
+         * points about the z-axis
+         */
+        float x = minus_one_one_dist(gen);
+        float y = minus_one_one_dist(gen);
+        float z = 0.0;
+        float mag = sqrtf(x * x + y * y + z * z);
+
+        noise[3 * i] = x / mag;
+        noise[3 * i + 1] = y / mag;
+        noise[3 * i + 2] = z / mag;
+    }
 
     float pos[3] = {0.0f};
     float angle[3] = {0.0f};
@@ -500,6 +728,11 @@ int main(int argc, char *argv[]) {
         }
 
         /*
+         *
+         */
+        glUseProgram(prog);
+
+        /*
          * Calculate world transform matrix based on current position and angle
          */
         glm::mat4 world;
@@ -523,15 +756,33 @@ int main(int argc, char *argv[]) {
         glUniformMatrix4fv(persp_unif, 1, GL_FALSE, &perspective[0][0]);
 
         /*
+         * Initial rendering pass:
+         * - diffuse textures with masking
+         * - surface normals
+         * - depth buffer
+         */
+        GLenum draw_buffers[] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1};
+
+        glBindFramebuffer(GL_FRAMEBUFFER, ssao_fbo);
+        glDrawBuffers(2, draw_buffers);
+
+        /*
          * Clear framebuffer
          */
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClearDepth(1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        glBindVertexArray(geometry_vao);
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+        glEnableVertexAttribArray(2);
         for (size_t i = 0; i < draw_objects.size(); i++) {
             glBindBuffer(GL_ARRAY_BUFFER, draw_objects[i].vbo);
 
+            /*
+             * TODO: only call these once per VBO
+             */
             glVertexAttribPointer(
                 0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
                 reinterpret_cast<void *>(offsetof(Vertex, position)));
@@ -553,7 +804,29 @@ int main(int argc, char *argv[]) {
             glBindTexture(GL_TEXTURE_2D, tex.mask);
 
             glDrawArrays(GL_TRIANGLES, 0, draw_objects[i].tri_count * 3);
+            ERROR_OPENGL("Error in initial pass (draw object %zu)", i);
         }
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+        glDisableVertexAttribArray(2);
+
+        /*
+         * Final pass: render texture to screen
+         */
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        ERROR_OPENGL("error binding default framebuffer");
+        glUseProgram(fullscreen_prog);
+
+        glBindVertexArray(fullscreen_vao);
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, fullscreen_vbo);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, ssao_tex);
+
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
 
         ERROR_OPENGL("checking for opengl error");
         glfwSwapBuffers(window);
